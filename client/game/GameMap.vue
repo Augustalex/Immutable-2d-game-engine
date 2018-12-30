a1285728aacf373f6b3f0cf634c3c91a8ad2bc2e
augustalex <augustalexandersson@gmail.com>
2018-12-30 01:34:57 +0100

    ...

--- client/game/GameMap.vue
+++ client/game/GameMap.vue
@@ -3,6 +3,13 @@
         <div class="biomes">
             <div v-for="biome in biomes" class="biome" :style="{backgroundImage: `url(/image/${biome.name}.jpg)` }">
                 <span class="biome-name">{{ biome.name }}</span>
+                <div class="biome-slots">
+                    <div v-for="_, index in 4"
+                         @click="biomeSlotClick(biome, index)"
+                         :class="getSlotClasses(biome, index)">
+                        {{ index }}
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -14,10 +21,57 @@
     } = Vuex.createNamespacedHelpers('game');
 
     module.exports = {
+        data() {
+            return {
+                selectedBiomeSlots: []
+            }
+        },
         computed: {
             ...mapState([
                 'biomes'
             ])
+        },
+        methods: {
+            getSlotClasses(biome, slotIndex) {
+                const classes = ['biome-slot'];
+                let alreadyHasSlotSelected = this.selectedBiomeSlots.some(s => {
+                    return s.biome.name === biome.name
+                        && s.slotIndex === slotIndex;
+                });
+                if (alreadyHasSlotSelected) {
+                    classes.push('biome-slot--selected');
+                }
+                return classes;
+            },
+            canSelectSlot(biome) {
+                let hasOtherBiomeSelected = this.selectedBiomeSlots.some(s => s.biome.name !== biome.name);
+                return !hasOtherBiomeSelected;
+            },
+            biomeSlotClick(biome, slotIndex) {
+                if (!this.canSelectSlot(biome, slotIndex)) return;
+
+                let alreadyHasSlotSelected = this.alreadyHasSlotSelected(biome, slotIndex)
+                if (alreadyHasSlotSelected) {
+                    this.selectedBiomeSlots = this.selectedBiomeSlots.filter(s => s.slotIndex !== slotIndex);
+                }
+                else {
+                    this.selectedBiomeSlots.push({ biome, slotIndex });
+                }
+
+                if (this.selectedBiomeSlots.length > 0) {
+                    const sortedByIndex = this.selectedBiomeSlots.slice().sort((a, b) => a.slotIndex - b.slotIndex);
+                    const isInSequence = sortedByIndex[sortedByIndex.length - 1].slotIndex - sortedByIndex[0].slotIndex === this.selectedBiomeSlots.length - 1;
+                    if (!isInSequence) {
+                        this.selectedBiomeSlots = alreadyHasSlotSelected ? [] : [{ biome, slotIndex }];
+                    }
+                }
+            },
+            alreadyHasSlotSelected(biome, slotIndex) {
+                return this.selectedBiomeSlots.some(s => {
+                    return s.biome.name === biome.name
+                        && s.slotIndex === slotIndex;
+                });
+            }
         }
     };
 </script>
